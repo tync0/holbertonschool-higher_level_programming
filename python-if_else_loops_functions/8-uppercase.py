f5d080ad17dcda83f8d2c5f9ad986d33d11f903f
tync0 <isgenderlituncay2004@gmail.com>
2024-01-27 08:43:15 -0800

    UPPERCASE

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) <= 122 and ord(i) >= 97:
+        if ord(i) < 123 and ord(i) > 96:
             i = chr(ord(i) - 32)
-        print("{}".format(i), end = "")
+        print("{}".format(i), end="")
     print()
