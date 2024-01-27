9686e22db3c258a839d570c6a7513756cecc5582
tync0 <isgenderlituncay2004@gmail.com>
2024-01-27 08:58:28 -0800

    Take Last Digit

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        x = -number % 10
+    else:
+        x = number % 10
+    print("{}".format(x), end = "")
+    return x
