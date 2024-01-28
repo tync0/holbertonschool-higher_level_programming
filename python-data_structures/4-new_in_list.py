22c698e146d89d6471a2d7ccfa27c3d9ceda5fcf
tync0 <isgenderlituncay2004@gmail.com>
2024-01-28 07:48:03 -0800

    Create new list and replace element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new = my_list.copy()
     if idx < 0 or idx >= len(my_list):
         return my_list
-    my_list[idx] = element
-    return my_list
+    new[idx] = element
+    return new
