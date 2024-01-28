4baa1f467925ba2da24032f13a76781b9520c5d9
tync0 <isgenderlituncay2004@gmail.com>
2024-01-28 10:33:16 -0800

    Find max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if !my_list:
+        return None
+    mx = my_list[0]
+    for i in my_list:
+        if mx < i:
+            mx = i
+    return mx
