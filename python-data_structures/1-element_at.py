b1ff0829868f0a63a4f2be00405a3699c7fc9c44
tync0 <isgenderlituncay2004@gmail.com>
2024-01-28 07:33:33 -0800

    Find index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return
-    else:
-        for i in my_list:
-            if i == idx:
-                return my_list[i]
+        return None
+    return my_list[idx]
