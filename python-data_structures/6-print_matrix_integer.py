4f7d4c58c4a81155f6228f37b40d0e55798a3b2a
tync0 <isgenderlituncay2004@gmail.com>
2024-01-28 09:51:23 -0800

    Print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in matrix:
+            for j in range(len(i)):
+                if j == len(i) - 1:
+                    print("{:d}".format(i[j]), end ="")
+                else:
+                    print("{:d}".format(i[j]), end =" ")
+            print()
