df0de8df33f21aa41898f6019a3f740d9627dc2b
tync0 <isgenderlituncay2004@gmail.com>
2024-01-28 05:43:15 -0800

    Print strings from another file

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for x in names:
+        if x[:2] == "__":
+            continue
+        print(x)
