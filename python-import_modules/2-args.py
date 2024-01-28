d7c1ac7b27e6addf5fd6615d8440c158c5c48acc
tync0 <isgenderlituncay2004@gmail.com>
2024-01-28 05:27:07 -0800

    Arguments in python

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv)
+if n == 1:
+    print("0 arguments.")
+elif n == 2:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(n - 1))
+for i in range(1, n):
+    print("{}: {}".format(i, sys.argv[i]))
