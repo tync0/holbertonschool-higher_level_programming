da336a5d5172377fabdbcc8c47b20d24187949d7
tync0 <isgenderlituncay2004@gmail.com>
2024-01-31 09:44:36 -0800

    Square of element of list

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-        new = []
-        for i in matrix:
-            row = [x ** 2 for x in i]
-            new.append(row)
-        return new
+    new = []
+    for i in matrix:
+        row = [x ** 2 for x in i]
+        new.append(row)
+    return new
