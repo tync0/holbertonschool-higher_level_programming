c561766cdc92be0f7c36db8775ca9989b5f9ba2f
tync0 <isgenderlituncay2004@gmail.com>
2024-01-31 09:49:12 -0800

    Replace element in llist

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = my_list.copy()
+    for i in range(len(new)):
+        if i == search:
+            new[i] = replace
+            return new
+    return new
