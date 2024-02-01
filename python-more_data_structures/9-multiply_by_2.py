4d2e77066dd88ec518539c6394793eb94754e057
tync0 <isgenderlituncay2004@gmail.com>
2024-02-01 03:12:39 -0800

    Multiple by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new = a_dictionary.copy()
     for key in a_dictionary.keys():
-        new[key] = a_dictionary[key] ** 2
+        new[key] = a_dictionary[key] * 2
     return new
