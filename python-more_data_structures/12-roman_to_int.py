d179262bb35d6de18b6cbfaf58b6b56d44b129c8
tync0 <isgenderlituncay2004@gmail.com>
2024-02-01 05:00:17 -0800

    Roman numbers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,19 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return None
+    roman_string = roman_string[::-1]
     a = 0
     numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
             "M": 1000}
+    keys = list(numbers.keys())
+    index = keys.index(roman_string[0])
+    print(index)
     for i in roman_string:
-        if i in numbers.keys():
-            a += numbers[i]
+        for j in range(len(keys)):
+            if i == keys[j]:
+                if index > j:
+                    a -= numbers[i]
+                else:
+                    a += numbers[i]
+                index = j
     return a
