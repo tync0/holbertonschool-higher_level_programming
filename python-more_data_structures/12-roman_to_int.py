23340bb3eac705ec0286bf754651069419fa15d6
tync0 <isgenderlituncay2004@gmail.com>
2024-02-01 05:10:21 -0800

    convert roman numbers to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not roman_string.isalpha():
+    if not roman_string or not isinstance(roman_string, str):
         return 0
     roman_string = roman_string[::-1]
     a = 0
-    numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
-            "M": 1000}
-    keys = list(numbers.keys())
+    keys = ["I", "V", "X", "L", "C", "D", "M"]
+    numbers = [1, 5, 10, 50, 100, 500, 1000]
     index = keys.index(roman_string[0])
     for i in roman_string:
         for j in range(len(keys)):
             if i == keys[j]:
                 if index > j:
-                    a -= numbers[i]
+                    a -= numbers[j]
                 else:
-                    a += numbers[i]
+                    a += numbers[j]
                 index = j
     return a
