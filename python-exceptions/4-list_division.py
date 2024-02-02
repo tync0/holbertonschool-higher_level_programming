7f2825eaf00595ff769a9e0ecd4754117348d7e5
tync0 <isgenderlituncay2004@gmail.com>
2024-02-02 11:12:11 -0800

    Create new list from division of 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,6 +14,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             a = 0
         finally:
-            new_list.append(elem)
+            new_list.append(a)
     return new_list
 
