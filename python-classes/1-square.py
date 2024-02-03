138e14842faad902662a162cf104c851bfbcadc0
tync0 <isgenderlituncay2004@gmail.com>
2024-02-03 11:50:44 -0800

    Size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a Square clas"""
+
+
+class Square:
+    """Document for Square class"""
+    def __init__(self, size):
+        """Document for init"""
+        self.__size = size
