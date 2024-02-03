98bc01d1cd0a7331b851ec1c379b5f297fc86dd4
tync0 <isgenderlituncay2004@gmail.com>
2024-02-03 11:56:08 -0800

    Give value to size

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a Square clas"""
+
+
+class Square:
+    """Document for Square class"""
+    def __init__(self, size = 0):
+        """Document for init"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
