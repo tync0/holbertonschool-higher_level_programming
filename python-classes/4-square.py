e5ff2409a9d4648b71a46ebe19942da4af015192
tync0 <isgenderlituncay2004@gmail.com>
2024-02-03 12:15:21 -0800

    Property and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Define a Square clas"""
+
+
+class Square:
+    """Document for Square class"""
+
+    def __init__(self, size=0):
+        """Document for init"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Document for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Document for setter size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Document for area"""
+        return self.__size ** 2
