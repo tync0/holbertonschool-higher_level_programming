dfeb00e27e67dde9ae8d306ea37ecebabe9db2b7
tync0 <isgenderlituncay2004@gmail.com>
2024-02-03 11:59:51 -0800

    Calculate area of Square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    
     def area(self):
         """Document for area"""
         return self.__size ** 2
