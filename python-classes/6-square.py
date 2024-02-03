3d370f3f3e0b250f04cacd169c772a84295c0942
tync0 <isgenderlituncay2004@gmail.com>
2024-02-03 12:39:40 -0800

    Add position attribute to Square class

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,7 +11,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
+        self.position  = position
 
     @property
     def size(self):
