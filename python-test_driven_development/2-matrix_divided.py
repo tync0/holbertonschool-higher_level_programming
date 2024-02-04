dd517d7d12e0ebc190c3576562b9d58379c85054
tync0 <isgenderlituncay2004@gmail.com>
2024-02-04 09:39:24 -0800

    Divide elements of matrix to div

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,22 +10,22 @@ def matrix_divided(matrix, div):
         elements of a matrix.
     """
 
-    if type(div) not in [int, float]:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(matrix) is not list:
         raise TypeError("matrix must be a matrix (list of lists) of "
-        "integers/floats")
-    if all(type(row) is list for row in matrix):
+                        "integers/floats")
+    if not all(type(row) is list for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of "
-        "integers/floats")
-    if all(len(row) != len(matrix[0]) for row in matrix):
+                        "integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    if (type(i) not in [int, float] for row in matrix for i in row):
+    if not all(type(i) in [int, float] for row in matrix for i in row):
         raise TypeError("matrix must be a matrix (list of lists) of "
-        "integers/floats")
-    new = [[]]
+                        "integers/floats")
+    new = []
     for row in matrix:
         new_row = []
         for i in row:
