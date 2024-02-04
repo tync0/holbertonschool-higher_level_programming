a79e258ae7287212e6923bbf0a66ae646f4b4eea
tync0 <isgenderlituncay2004@gmail.com>
2024-02-04 05:25:15 -0800

    first commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""A module that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """2 integer addition"""
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
