210725cceccd7543a6f69d51653d94c18039c8ba
tync0 <isgenderlituncay2004@gmail.com>
2024-02-04 06:24:54 -0800

    print full name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,8 +9,8 @@ def say_my_name(first_name, last_name=""):
         say_my_name - Prints My name is <first name> <last name>
     """
 
-    if not type(first_name) is str:
-        raise TypeError("irst_name must be a string")
-    if not type(last_name) is str:
-        raise TypeError("ast_name must be a string")
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
