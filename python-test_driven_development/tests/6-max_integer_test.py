74efda4c4d4aca5fdb361ac20a9a82803441272f
tync0 <isgenderlituncay2004@gmail.com>
2024-02-04 09:56:19 -0800

    UnitTest for finding max element of list

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+max_integer = __import__("6-max_integer").max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_positive(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_negative(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+
+    def test_empty(self):
+        self.assertEqual(max_integer([]))
+    
+    def test_one_number(self):
+        self.assertEqual(max_integer([2]), 2)
+
+    def test_both_numbers(self):
+        self.assertEqual(max_integer([1, -2, 3, -4]), 3)
+        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
