5511749c4d48cfe9bda6c57a65cdf7748d2472f3
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 15:41:25 +0400

    task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,6 +78,6 @@ class Rectangle:
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area >= rect_2.area:
-            return rect_1.area
+            return rect_1
         else:
-            return rect_2.area
+            return rect_2
