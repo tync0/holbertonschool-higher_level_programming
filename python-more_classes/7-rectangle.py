cc85da2aa41a4d54c891c304a150659b52c4a7bf
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 15:26:35 +0400

    tasks done

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -8,6 +8,14 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         Rectangle.number_of_instances += 1
         self.__width = width
         self.__height = height
