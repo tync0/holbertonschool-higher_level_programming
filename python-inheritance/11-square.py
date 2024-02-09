e0c3ae067004d1f27e7f6cd09e30149ab49357e5
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 22:13:04 +0400

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""Module doc"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Rectangle class"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def __str__(self):
+        print("[Square] {}/{}".format(self.__size, self.__size))
