55665e14994e39ea716e817f4725b59ee33fe3d3
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 15:29:51 +0400

    task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -48,7 +48,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ("")
