2bfa0fd57b1195f572c7ba1b0adc9ec0a7628255
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 21:48:00 +0400

    Check inheritance of class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""Module doc"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Same
+    """
+    if isinstance(obj, a_class) or type(obj) is a_class:
+        return True
+    return False
