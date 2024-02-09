228514f2e341e477796262431d2ba82732b2bae9
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 15:35:15 +0400

    Task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle:
         Rectangle.number_of_instances += 1
         self.__width = width
         self.__height = height
+
     @property
     def width(self):
         return self.__width
@@ -49,7 +50,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ("")
@@ -60,7 +61,7 @@ class Rectangle:
             if not i == self.height - 1:
                 new += "\n"
         return new
-    
+
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
     
