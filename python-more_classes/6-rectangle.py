0b787da79ef33ee6cd1576fa119a7300f19c85fb
tync0 <isgenderlituncay2004@gmail.com>
2024-02-09 15:38:32 +0400

    tasks done

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """defined Rectangle"""
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
@@ -53,7 +54,7 @@ class Rectangle:
 
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return ("")
+            return ""
         new = ""
         for i in range(self.height):
             for j in range(self.width):
@@ -64,7 +65,7 @@ class Rectangle:
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """defined Rectangle"""
+
     number_of_instances = 0
     print_symbol = "#"
 
@@ -19,6 +20,7 @@ class Rectangle:
         Rectangle.number_of_instances += 1
         self.__width = width
         self.__height = height
+
     @property
     def width(self):
         return self.__width
@@ -50,10 +52,10 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return ("")
+            return ""
         new = ""
         for i in range(self.height):
             for j in range(self.width):
@@ -61,14 +63,14 @@ class Rectangle:
             if not i == self.height - 1:
                 new += "\n"
         return new
-    
+
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) is not Rectangle:
