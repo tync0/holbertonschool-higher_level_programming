f4534a157a91b0cc3225c6a8b5ec64ebcdd4cb25
tync0 <isgenderlituncay2004@gmail.com>
2024-02-11 19:11:28 +0400

    add new feature to to_json function

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""Documented Module"""
+
+
+class Student:
+    """Documented Module"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                new_dict[i] = self.__dict__[i]
+        return new_dict
