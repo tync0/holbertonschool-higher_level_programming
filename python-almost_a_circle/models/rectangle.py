116ec5d07b892abec503b0e795f2af59970f5e57
tync0 <isgenderlituncay2004@gmail.com>
2024-02-14 09:48:35 +0400

    Rectangle class from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rectangle class that inherits from Base class."""
+from base import Base
+
+
+class Rectangle(Base):
+    """Represents the Rectangle class."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes an instance of the Rectangle class.
+
+        Args:
+            width (int): The width of the Rectangle.
+            height (int): The height of the Rectangle.
+            x (int): The x coordinate of the Rectangle.
+            y (int): The y coordinate of the Rectangle.
+            id (int): The identity of the new Rectangle.
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super.__init__(id)
