12c02a3702416d5bcdeb628bf3f21e866819ac03
tync0 <isgenderlituncay2004@gmail.com>
2024-02-14 09:57:50 +0400

    some bugs fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,8 +16,44 @@ class Rectangle(Base):
             y (int): The y coordinate of the Rectangle.
             id (int): The identity of the new Rectangle.
         """
+        super.__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super.__init__(id)
+
+    @property
+    def width(self):
+        """set/get methods for the width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """set/get method for the height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """set/get methods for the x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        """set/get methods for the y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
