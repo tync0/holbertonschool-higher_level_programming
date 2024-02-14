824a89ec3ae154b9c1d0197e2876bb31a3096fa1
tync0 <isgenderlituncay2004@gmail.com>
2024-02-14 15:00:41 +0400

    to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Represents the Base class."""
 
+import json
+
 
 class Base:
     """Represents the Base class.
@@ -24,3 +26,10 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """"""
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        return "[]"
