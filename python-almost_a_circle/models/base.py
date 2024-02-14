78655bc98f9a33066c65cb7b2727532e32556b9f
tync0 <isgenderlituncay2004@gmail.com>
2024-02-14 09:33:25 +0400

    first commit

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+class Base:
+    """Represents the Base class.
+
+    Base class for all other classes in the model package.
+
+    Attributes:
+        __nb_objects (int): The number of objects created.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes an instance of the Base class.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
